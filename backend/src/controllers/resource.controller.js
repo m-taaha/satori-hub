49fe3d95b00de9735bc77008f1756dbeb39635cb
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-05 22:06:50 +0530

    feat: add get All resources for loggedIn user

--- backend/src/controllers/resource.controller.js
+++ backend/src/controllers/resource.controller.js
@@ -35,3 +35,15 @@ export const uploadResource = async (req, res) => {
         })
     }
 }
+
+export const getAllResources = async (req, res) => {
+    try{
+        const resources = await Resource.find({author: req.user._id}).sort({createdAt: -1})
+        return res.status(200).json({resources});
+    }catch(error){
+        console.log(`Getting All Resources Error:`, error.message);
+        return res.status(500).json({
+          message: `Server Error`,
+        });
+    }
+}
--- backend/src/routes/resource.routes.js
+++ backend/src/routes/resource.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
-import { uploadResource } from "../controllers/resource.controller.js";
+import { getAllResources, uploadResource } from "../controllers/resource.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { uploadResourceSchema } from "../validators/resource.validator.js";
 import validate from "../middlewares/validate.middleware.js";
 
 const resourceRouter = Router();
 
+//route to upload
 resourceRouter.post("/upload", 
     isAuthenticated, 
-    validate(uploadResourceSchema) ,uploadResource)
+    validate(uploadResourceSchema) ,uploadResource);
+
+//route to get your own all resources
+    resourceRouter.get("/my-resources", isAuthenticated,  getAllResources);
 
 
 export default resourceRouter;
