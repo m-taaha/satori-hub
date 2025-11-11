eb344f61111f3bcc7a1d818c1f82251d0aba6245
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-11 23:54:17 +0530

    feat: add search my resources

--- backend/src/controllers/resource.controller.js
+++ backend/src/controllers/resource.controller.js
@@ -1,4 +1,7 @@
+import { file } from "zod";
 import { Resource } from "../models/resource.model.js"
+
+//uploading resources
 export const uploadResource = async (req, res) => {
     try{
 
@@ -36,6 +39,7 @@ export const uploadResource = async (req, res) => {
     }
 }
 
+//get all resources
 export const getAllResources = async (req, res) => {
     try{
         const resources = await Resource.find({author: req.user._id}).sort({createdAt: -1})
@@ -45,5 +49,34 @@ export const getAllResources = async (req, res) => {
         return res.status(500).json({
           message: `Server Error`,
         });
+    }
+}
+
+export const searchMyResources = async (req, res) => {
+    try{
+        const {search, category, difficulty, resourceType} = req.query;
+
+        let filter = {author: req.user._id}; // only their own resources they'll get
+        
+        if(category) filter.category = category;
+        if(difficulty) filter.difficulty = difficulty;
+        if(resourceType) filter.resourceType = resourceType;
+
+        if(search){
+            filter.$or = [
+                {title: {$regex: search, $options: "i"}},
+                {description: { $regex: search, $options: "i"}}
+            ];
+        }
+        
+        const resources = (await Resource.find(filter)).toSorted({createdAt: -1});
+        return res.status(200).json({resources})
+
+
+    } catch (error){
+        console.log('Error in Seaching My Resources:', error.message);
+        return res.status(500).json({
+            message: `Server Error`,
+        })
     }
 }
--- backend/src/routes/resource.routes.js
+++ backend/src/routes/resource.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllResources, uploadResource } from "../controllers/resource.controller.js";
+import { getAllResources, searchMyResources, uploadResource } from "../controllers/resource.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { uploadResourceSchema } from "../validators/resource.validator.js";
 import validate from "../middlewares/validate.middleware.js";
@@ -12,7 +12,11 @@ resourceRouter.post("/upload",
     validate(uploadResourceSchema) ,uploadResource);
 
 //route to get your own all resources
-    resourceRouter.get("/my-resources", isAuthenticated,  getAllResources);
+resourceRouter.get("/my-resources", isAuthenticated,  getAllResources);
+
+//route to get resource based on search
+resourceRouter.get("/my-resources/search", isAuthenticated, searchMyResources)
+
 
 
 export default resourceRouter;
