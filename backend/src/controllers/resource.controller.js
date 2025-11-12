0231e6d3f909185d323e8a55b16f606fb152bc41
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-12 20:46:11 +0530

    feat: add public routes and controller to search and get all resources

--- backend/src/controllers/resource.controller.js
+++ backend/src/controllers/resource.controller.js
@@ -1,140 +1,191 @@
-
-import { Resource } from "../models/resource.model.js"
+import { Resource } from "../models/resource.model.js";
 
 //uploading resources
 export const uploadResource = async (req, res) => {
-    try{
+  try {
+    const {
+      title,
+      description,
+      difficulty,
+      category,
+      thumbnailImage,
+      resourceType,
+      resourceLink,
+    } = req.body;
 
-        const { 
-            title, 
-            description, 
-            difficulty, 
-            category, 
-            thumbnailImage, 
-            resourceType, 
-            resourceLink
-        } = req.body;
+    const newResource = await Resource.create({
+      title,
+      description,
+      difficulty,
+      category,
+      thumbnailImage,
+      resourceType,
+      resourceLink,
+      author: req.user._id, // user come from auth-middleware
+    });
 
-        const newResource = await Resource.create({
-            title,
-            description,
-            difficulty,
-            category,
-            thumbnailImage,
-            resourceType,
-            resourceLink,
-            author: req.user._id // user come from auth-middleware 
-        })
-
-        return res.status(201).json({
-            message: "Resource Uploaded Successfully",
-            resource: newResource
-        })
-
-    }catch(error){
-        console.log(`Uploading Error:`, error.message)
-        return res.status(500).json({
-            message: `Server Error`
-        })
-    }
-}
+    return res.status(201).json({
+      message: "Resource Uploaded Successfully",
+      resource: newResource,
+    });
+  } catch (error) {
+    console.log(`Uploading Error:`, error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
 
 //get all resources
-export const getAllResources = async (req, res) => {
-    try{
-        const resources = await Resource.find({author: req.user._id}).sort({createdAt: -1})
-        return res.status(200).json({resources});
-    }catch(error){
-        console.log(`Getting All Resources Error:`, error.message);
-        return res.status(500).json({
-          message: `Server Error`,
-        });
-    }
-}
+export const getMyResources = async (req, res) => {
+  try {
+    const resources = await Resource.find({ author: req.user._id }).sort({
+      createdAt: -1,
+    });
+    return res.status(200).json({ resources });
+  } catch (error) {
+    console.log(`Getting All Resources Error:`, error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
 
 //search my own resources
 export const searchMyResources = async (req, res) => {
-    try{
-        const {search, category, difficulty, resourceType} = req.query;
+  try {
+    const { search, category, difficulty, resourceType } = req.query;
 
-        let filter = {author: req.user._id}; // only their own resources they'll get
-        
-        if(category) filter.category = category;
-        if(difficulty) filter.difficulty = difficulty;
-        if(resourceType) filter.resourceType = resourceType;
+    let filter = { author: req.user._id }; // only their own resources they'll get
 
-        if(search){
-            filter.$or = [
-                {title: {$regex: search, $options: "i"}},
-                {description: { $regex: search, $options: "i"}}
-            ];
-        }
-        
-        const resources = (await Resource.find(filter)).sort({createdAt: -1});
-        return res.status(200).json({resources})
+    if (category) filter.category = category;
+    if (difficulty) filter.difficulty = difficulty;
+    if (resourceType) filter.resourceType = resourceType;
 
-
-    } catch (error){
-        console.log('Error in Seaching My Resources:', error.message);
-        return res.status(500).json({
-            message: `Server Error`,
-        })
+    if (search) {
+      filter.$or = [
+        { title: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
     }
-}
+
+    const resources = (await Resource.find(filter)).sort({ createdAt: -1 });
+    return res.status(200).json({ resources });
+  } catch (error) {
+    console.log("Error in Seaching My Resources:", error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
 
 //upadting the resources
 export const updateResource = async (req, res) => {
-    try{
-        const {id} = req.params;
-        const updates = req.body;
+  try {
+    const { id } = req.params;
+    const updates = req.body;
 
-        //only update if the resource belong the logged in user
-        const updated = await Resource.findOneAndUpdate(
-            {_id: id, author: req.user._id},
-            updates,
-            {new: true}
-        );
+    //only update if the resource belong the logged in user
+    const updated = await Resource.findOneAndUpdate(
+      { _id: id, author: req.user._id },
+      updates,
+      { new: true }
+    );
 
-        if(!updated) {
-            return res.status(404).json({
-                message: "Resource not found found or unauthorized"
-            })
-        }
-
-        return res.status(200).json({
-            message: "Resource updated successfully",
-            resource: updated
-        });
-
-    }catch(error){
-        console.log("Update Error:", error.message);
-        return res.status(500).json({
-          message: `Server Error`,
-        });
+    if (!updated) {
+      return res.status(404).json({
+        message: "Resource not found found or unauthorized",
+      });
     }
-}
 
-//deleting resources 
+    return res.status(200).json({
+      message: "Resource updated successfully",
+      resource: updated,
+    });
+  } catch (error) {
+    console.log("Update Error:", error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
+
+//deleting resources
 export const deleteResource = async (req, res) => {
-    try{
-        const {id} = req.params;
+  try {
+    const { id } = req.params;
 
-        const resource = await Resource.findOneAndDelete({_id: id, author: req.user._id});
+    const resource = await Resource.findOneAndDelete({
+      _id: id,
+      author: req.user._id,
+    });
 
-        if(!resource){
-            return res.status(404).json({
-                message: "Resource not Found"
-            });
-        }
-
-        return res.status(200).json({
-            message: "Resource Deleted successfully",
-            deletedResource: id,
-        })
-    }catch(error){
-          console.log("Delete Error:", error.message);
-          return res.status(500).json({
-            message: `Server Error`,
-          });
+    if (!resource) {
+      return res.status(404).json({
+        message: "Resource not Found",
+      });
     }
-}
+
+    return res.status(200).json({
+      message: "Resource Deleted successfully",
+      deletedResource: id,
+    });
+  } catch (error) {
+    console.log("Delete Error:", error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
+
+//public controllers
+//to get all resources publically
+export const getAllResources = async (req, res) => {
+  try {
+    const resources = (await Resource.find()).toSorted({createdAt: -1});
+
+    if (!resources) {
+      return res.status(404).json({
+        message: "No Resources Found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Available Resources",
+      resources: resources,
+    });
+  } catch (error) {
+    console.log("get All Resources Error:", error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
+
+//search resources
+export const searchResources = async (req, res) => {
+  try {
+    const { search, category, difficulty, resourceType } = req.query;
+
+    let filter = {}; 
+
+    if (category) filter.category = category;
+    if (difficulty) filter.difficulty = difficulty;
+    if (resourceType) filter.resourceType = resourceType;
+
+    if (search) {
+      filter.$or = [
+        { title: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    const resources = (await Resource.find(filter)).sort({ createdAt: -1 });
+    return res.status(200).json({ resources });
+  } catch (error) {
+    console.log("Error in Seaching My Resources:", error.message);
+    return res.status(500).json({
+      message: `Server Error`,
+    });
+  }
+};
--- backend/src/routes/resource.routes.js
+++ backend/src/routes/resource.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteResource, getAllResources, searchMyResources, updateResource, uploadResource } from "../controllers/resource.controller.js";
+import { deleteResource, getAllResources, getMyResources, searchMyResources, searchResources, updateResource, uploadResource } from "../controllers/resource.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { uploadResourceSchema } from "../validators/resource.validator.js";
 import validate from "../middlewares/validate.middleware.js";
@@ -12,7 +12,7 @@ resourceRouter.post("/upload",
     validate(uploadResourceSchema) ,uploadResource);
 
 //route to get your own all resources
-resourceRouter.get("/my-resources", isAuthenticated,  getAllResources);
+resourceRouter.get("/my-resources", isAuthenticated,  getMyResources);
 
 //route to get resource based on search
 resourceRouter.get("/my-resources/search", isAuthenticated, searchMyResources);
@@ -22,6 +22,15 @@ resourceRouter.put("/update/:id", isAuthenticated, updateResource);
 
 //route to delete your resouces
 resourceRouter.delete("/delete/:id", isAuthenticated, deleteResource);
+
+
+// routes for public use for all users
+
+// route to get all resources
+resourceRouter.get('/resources', getAllResources)
+
+//route to get search resources
+resourceRouter.get('/resources/search', searchResources)
 
 
 
