57cd22705e6eb4865aca7ff0db19c1240cbec2b4
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-13 21:14:50 +0530

    fix: review schema

--- backend/src/models/review.model.js
+++ backend/src/models/review.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const reviewSchema = new mongoose.Schema({
     rating: {
         type: Number,
-        required: true,
+        min: 1,
+        max: 5
     }, 
     comment: {
         type: String,
