b907df9dfe2304496b8a790b618d2b94d223e105
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-13 21:36:07 +0530

    fix: review model and review validator

--- backend/src/models/review.model.js
+++ backend/src/models/review.model.js
@@ -4,7 +4,8 @@ const reviewSchema = new mongoose.Schema({
     rating: {
         type: Number,
         min: 1,
-        max: 5
+        max: 5,
+        required: true,
     }, 
     comment: {
         type: String,
--- backend/src/validators/review.validator.js
+++ backend/src/validators/review.validator.js
@@ -5,7 +5,8 @@ export const createReviewSchema = z.object({
     .string()
     .trim()
     .min(1, { message: "Comment cannot be empty" })
-    .max(500, { message: "Comment too long (max 500 chars)" }),
+    .max(500, { message: "Comment too long (max 500 chars)" })
+    .optional(), // optional if user might not give rating,
   rating: z
     .number()
     .min(1, { message: "Minimum rating is 1" })
