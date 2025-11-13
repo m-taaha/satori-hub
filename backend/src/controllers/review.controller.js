db4fa2d5ce6fe28759666902a44484d7f295d479
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-13 23:09:23 +0530

    fix: update createReview controller

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -1,21 +1,41 @@
 import {Review} from "../models/review.model.js"
+import {Resource} from "../models/resource.model.js"
 
 export const createReview = async (req, res) => {
     try{
-         const {rating, comment } = req.body;
-         const { resourceId } = req.params;
+      const { rating, comment } = req.body;
+      const { resourceId } = req.params;
+      const userId = req.user._id;
 
-         const review = await Review.create({
-          comment,
-          rating,
-          user: req.user._ud,
-          resource: resourceId,
-         });
+      //checking if resource exist or not
+      const resource = await Resource.findById(resourceId);
+      if (!resource) {
+        return res.status(404).json({ message: "Resource not found" });
+      }
 
-         return res.status(201).json({
-          message: "Review Added Successfully",
-          review,
-         });         
+      //check if user has already reviwed this resource
+      // We look for a review where 'user' is ME and 'resource' is THIS ONE
+      const exitngReview = await Review.findOne({
+          user: userId,
+          resource: resourceId
+      })
+
+      if(exitngReview){
+          return res.status(409).json({message: "You have already reviewed this resource "})
+      }
+
+      //create the new review 
+      const newReview = await Review.create({
+        comment,
+        rating,
+        user: userId,
+        resource: resourceId,
+      });
+
+      return res.status(201).json({
+        message: "Review Added Successfully",
+        review: newReview,
+      });
     } catch(error) {
          console.log("Create Review Error", error);
          return res.status(500).json({
