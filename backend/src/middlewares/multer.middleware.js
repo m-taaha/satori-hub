48def5a3c9f821ebaa2850f4b289cfa3ac9092cf
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-15 01:35:07 +0530

    feat: add multer middleware

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+//telling multer to save the file on disc (on our server)
+const storage = multer.diskStorage({
+    //'destination' is a funciton that tells multer where to put the files
+    destination: function (req, file, cb) {
+        //we'll create this '/public/temp' folder in our root directory
+        cb(null, "./public/temp"); 
+    },
+
+    //'filename' tells the multer what to call the file
+    filename: function (req, file, cb) {
+        //for now we'll just keep the original file name
+        //in a real app, you might add  a uniqye prefix to avoid conflicts
+        cb(null, file.originalname );
+    },
+});
+
+export const upload = multer({
+    storage: storage,
+});
