5482b99f7099d1f87b51cfe914f696e809afadab
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-15 22:47:02 +0530

    feat: (fronted) set skeleton home routes and rootlayout

---
+++ frontend/src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+function RootLayout() {
+  return (
+    <div>
+        //we'll add a navbar later here
+        <main>
+            <Outlet />. //this is where (Home,login) pages will be rendered here
+        </main>
+        //we'll add a footer here later
+    </div>
+  )
+}
+
+export default RootLayout; 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
+//define all the routes of app here
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/> , //all the pages will live inside this layout
+    children: [
+      {
+        index: true, //this makes HOME the default page for "/"
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+      //dashboard and all i'll add here later
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
